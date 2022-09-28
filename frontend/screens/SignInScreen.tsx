3d135307e7aa8b30f5bba738c68b322de474b32a
TucoElGrande <bjorndjosefsson@gmail.com>
2022-09-28 15:36:49 +0200

    add simple haptics onregister

--- frontend/screens/SignInScreen.tsx
+++ frontend/screens/SignInScreen.tsx
@@ -1,4 +1,3 @@
-import { useNavigation } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { useForm } from "react-hook-form";
