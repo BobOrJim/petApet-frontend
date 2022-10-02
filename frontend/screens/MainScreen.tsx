b1a643f296039162cc10ebde27b23784d9cf1204
Robin Keturi <Robin.Keturi@gmail.com>
2022-10-02 17:58:04 +0200

    added addAdvertButton and removed the old one

--- frontend/screens/MainScreen.tsx
+++ frontend/screens/MainScreen.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import { FlashList } from "@shopify/flash-list";
 import AdvertListItem from "../components/AdvertComponents/AdvertListItem";
 import AdvertSearchfilters from "../components/AdvertComponents/AdvertSearchfilters";
+import { FAB } from "react-native-paper";
+import { useUserContext } from "../contexts/UserContext";
 
 export type SearchParams = {
   searchAll: string;
@@ -16,31 +18,42 @@ export default function MainScreen({ navigation }: Props) {
   const [refreshing] = useState(false);
   const [searchFilters, setSearchfilters] = useState<SearchParams>({ searchAll: "" });
   const { adverts, getAllAdverts } = useAdverts();
+  const { user } = useUserContext();
   return (
-    <FlashList
-      ListHeaderComponent={
-        <AdvertSearchfilters
-          navigation={navigation}
-          searchFilters={searchFilters}
-          setSearchfilters={setSearchfilters}
+    <>
+      <FlashList
+        ListHeaderComponent={
+          <AdvertSearchfilters
+            searchFilters={searchFilters}
+            setSearchfilters={setSearchfilters}
+          />
+        }
+        contentContainerStyle={{ paddingTop: 10 }}
+        onRefresh={getAllAdverts}
+        refreshing={refreshing}
+        data={adverts.filter((advert) => {
+          for (const value of Object.values(advert)) {
+            if (
+              (typeof value === "string" &&
+                value.toLowerCase().includes(searchFilters.searchAll.toLowerCase())) ||
+              (typeof value === "number" && value === Number(searchFilters.searchAll))
+            ) {
+              return true;
+            }
+          }
+        })}
+        estimatedItemSize={100}
+        renderItem={({ item }) => <AdvertListItem advert={item} navigation={navigation} />}
+      />
+      {
+      user && 
+        <FAB
+          size="small"
+          icon="plus"
+          style={{ position: 'absolute', margin: 16, right: 0, bottom: 0 }}
+          onPress={() => navigation.navigate("AddAdvert")}
         />
       }
-      contentContainerStyle={{ paddingTop: 10 }}
-      onRefresh={getAllAdverts}
-      refreshing={refreshing}
-      data={adverts.filter((advert) => {
-        for (const value of Object.values(advert)) {
-          if (
-            (typeof value === "string" &&
-              value.toLowerCase().includes(searchFilters.searchAll.toLowerCase())) ||
-            (typeof value === "number" && value === Number(searchFilters.searchAll))
-          ) {
-            return true;
-          }
-        }
-      })}
-      estimatedItemSize={100}
-      renderItem={({ item }) => <AdvertListItem advert={item} navigation={navigation} />}
-    />
+    </>
   );
 }
