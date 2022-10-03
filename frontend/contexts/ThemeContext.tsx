fd3f0d8a0f0253b85c79c274d6869bb129999b72
Robin Keturi <Robin.Keturi@gmail.com>
2022-10-03 16:01:28 +0200

    save theme preference and custom theme in secure storage

--- frontend/contexts/ThemeContext.tsx
+++ frontend/contexts/ThemeContext.tsx
@@ -3,48 +3,63 @@ import { createContext, Dispatch, ReactNode, useContext, useEffect, useState } f
 import { MD3LightTheme as PaperDefaultTheme, MD3DarkTheme as PaperDarkTheme} from "react-native-paper"
 import { DefaultTheme as NavigationDefaultTheme, DarkTheme as NavigationDarkTheme, Theme } from "@react-navigation/native"
 import merge from "deepmerge";
+import useSecureStorage from "../hooks/useSecureStorage";
 
 interface ContextValue {
     currentTheme: Theme;
     darkmode: boolean;
-    setDarkMode: Dispatch<React.SetStateAction<boolean>>
+    setDarkmode: Dispatch<React.SetStateAction<boolean>>
     usingCustomTheme: boolean;
     setUsingCustomTheme: Dispatch<React.SetStateAction<boolean>>
-    createCustomTheme: (card: string, background: string, surface: string) => void;
+    customColors: CustomColors;
+    setCustomColors: Dispatch<React.SetStateAction<CustomColors>>
 }
 
 const ThemeContext = createContext<ContextValue>({} as ContextValue);
+
+interface CustomColors {
+  card: string;
+  background: string;
+  surface: string;
+}
 
 interface Props {
   children: ReactNode;
 }
 
 export default function ThemeProvider({ children }: Props) {
-    const [darkmode, setDarkMode] = useState(false);
-    const [usingCustomTheme, setUsingCustomTheme] = useState(false);
-    const [currentTheme, setCurrentTheme] = useState<Theme>(StandardDarkTheme);
+    const [darkmode, setDarkmode] = useSecureStorage<boolean>("darkmode", false);
+    const [currentTheme, setCurrentTheme] = useState<Theme>(darkmode ? StandardDarkTheme : StandardLightTheme);
+    const [usingCustomTheme, setUsingCustomTheme] = useSecureStorage("usingCustomTheme", false);
+    const [customColors, setCustomColors] = useSecureStorage<CustomColors>("customColors", {
+      card: "#fff",
+      background: "#fff",
+      surface: "#fff"
+    });
+    
+    useEffect(() => {
+        if(!usingCustomTheme){
+          setCurrentTheme(darkmode ? StandardDarkTheme : StandardLightTheme)
+      }
+    }, [darkmode, usingCustomTheme])
 
     useEffect(() => {
-        setCurrentTheme(darkmode ? StandardDarkTheme : StandardLightTheme)
-    }, [darkmode])
-
-    function createCustomTheme(card: string, background: string, surface: string){     
-      setCurrentTheme(prevState => ({
-            ...prevState,
-            background: background !== "" ? background : prevState.colors.background,
-            surface: surface !== "" ? surface : prevState.colors.surface,
-            colors: {
-                ...prevState.colors,
-                background: background ? background : prevState.colors.background,
-                surface: surface ? surface : prevState.colors.surface,
-                card: card ? card : prevState.colors.card
-            }
-      }))
-    }
+      if(customColors !== undefined) {
+        setCurrentTheme(prevState => ({
+              ...prevState,
+              colors: {
+                  ...prevState.colors,
+                  background: customColors.background !== undefined ? customColors.background : prevState.colors.background,
+                  surface: customColors.surface !== undefined ? customColors.surface : prevState.colors.surface,
+                  card: customColors.card !== undefined ? customColors.card : prevState.colors.card
+              }
+        }))
+      }
+    }, [customColors])
 
   return (
     <ThemeContext.Provider
-      value={{ currentTheme, darkmode, setDarkMode, createCustomTheme, usingCustomTheme, setUsingCustomTheme}}
+      value={{ currentTheme, darkmode, setDarkmode, customColors, setCustomColors, usingCustomTheme, setUsingCustomTheme }}
     >
       {children}
     </ThemeContext.Provider>
