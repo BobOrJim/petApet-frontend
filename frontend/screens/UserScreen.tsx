61b2545b37ef7b4c3d73f615be1f597cf489108f
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2022-10-05 20:15:21 +0200

    iosUserScreen

--- frontend/screens/UserScreen.tsx
+++ frontend/screens/UserScreen.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, ScrollView } from "react-native";
 import { Button, Text, Title } from "react-native-paper";
 import CustomButton from "../components/CustomButton/CustomButton";
 import CustomInput from "../components/CustomInput/CustomInput";
@@ -10,8 +10,9 @@ import { User } from "../models/User";
 import { RootStackParamList } from "../NavigationContainerContainer";
 import { IMGURL_REGEX } from "./AddAdvertScreen";
 import { EMAIL_REGEX } from "./SignUpScreen";
+import Ios_extraKeyboardPadding from "../components/Ios_extraKeyboardPadding";
 
-export default function UserScreen({ navigation } : NativeStackScreenProps<RootStackParamList>) {
+export default function UserScreen({ navigation }: NativeStackScreenProps<RootStackParamList>) {
   const { user, updateUser, DeleteLoggedInUser, LogOutUser } = useUserContext();
   const [editMode, setEditMode] = useState(false);
   const { control, handleSubmit } = useForm<any>({});
@@ -25,125 +26,126 @@ export default function UserScreen({ navigation } : NativeStackScreenProps<RootS
         profilePictureUrl: data.profilePictureUrl ? data.profilePictureUrl : user.profilePictureUrl,
         alias: data.alias ? data.alias : user.alias,
         contactEmail: data.contactEmail ? data.contactEmail : user.contactEmail,
-      })
+      });
 
-      if(result) {
-        alert("User updated!")
-        setEditMode(false)
+      if (result) {
+        alert("User updated!");
+        setEditMode(false);
       }
     }
   }
 
   async function deleteAccount() {
     const result = await DeleteLoggedInUser();
-    navigation.navigate("Main");  
+    navigation.navigate("Main");
   }
 
   async function handleLogOut() {
     const result = await LogOutUser();
-    navigation.navigate("Main");  
+    navigation.navigate("Main");
   }
 
   return (
-    <View
-      style={{ ...styles.container, justifyContent: editMode ? "space-between" : "space-evenly" }}
-    >
-      <Image
-        style={styles.profilePicture}
-        source={{
-          uri: user?.profilePictureUrl
-            ? user?.profilePictureUrl
-            : "https://www.pngkey.com/png/full/73-730477_first-name-profile-image-placeholder-png.png",
+    <Ios_extraKeyboardPadding>
+      <ScrollView
+        contentContainerStyle={{
+          ...styles.container,
+          justifyContent: editMode ? "space-between" : "space-evenly",
         }}
-      />
-      {editMode ? (
-        <View>
-          <CustomInput
-            defaultValue={user?.alias}
-            name='alias'
-            placeholder='Alias'
-            control={control}
-            keyboardType={"default"}
-            rules={{
-              minLength: {
-                value: 3,
-                message: "Alias must be minimum 3 characters long",
-              },
-            }}
-          />
-          <CustomInput
-            defaultValue={user?.phoneNr}
-            name='phoneNr'
-            placeholder='Phone number'
-            control={control}
-            keyboardType={"numeric"}
-            rules={{
-              pattern: { value: /^[+]?\d{8,12}$/, message: "Must be valid phone number" },
-            }}
-          />
-          <CustomInput
-            defaultValue={user?.contactEmail}
-            name='contactEmail'
-            placeholder='Email'
-            control={control}
-            keyboardType={"default"}
-            rules={{
-              pattern: { value: EMAIL_REGEX, message: "Must be a valid email adress" },
-            }}
-          />
-          <CustomInput
-            defaultValue={user?.profilePictureUrl}
-            placeholder='Profile picture url'
-            name='profilePictureUrl'
-            keyboardType={"url"}
-            control={control}
-            rules={{
-              pattern: { value: IMGURL_REGEX, message: "Must be a valid url adress" },
-            }}
-          />
-          <View style={{ justifyContent: "center", marginLeft: "auto", marginRight: "auto" }}>
-            <CustomButton text='Update profile' onPress={handleSubmit(onSubmit)} />
-            <CustomButton
-              text='Discard changes'
-              bgColor='#c50f1f'
-              onPress={() => setEditMode(false)}
-            />
-          </View>
-        </View>
-      ) : (
-        <View>
-          <Title>{user?.alias}</Title>
-          {user?.contactEmail && <Text variant='titleSmall'>Email: {user?.contactEmail}</Text>}
-          {user?.phoneNr && <Text variant='titleSmall'>Phone number: {user?.phoneNr}</Text>}
-        </View>
-      )}
-
-      <View style={styles.buttonContainer}>
+      >
+        <Image
+          style={styles.profilePicture}
+          source={{
+            uri: user?.profilePictureUrl
+              ? user?.profilePictureUrl
+              : "https://www.pngkey.com/png/full/73-730477_first-name-profile-image-placeholder-png.png",
+          }}
+        />
         {editMode ? (
-          <Button mode='contained' buttonColor='#c50f1f' onPress={deleteAccount}>
-            Delete account
-          </Button>
+          <View>
+            <CustomInput
+              defaultValue={user?.alias}
+              name='alias'
+              placeholder='Alias'
+              control={control}
+              keyboardType={"default"}
+              rules={{
+                minLength: {
+                  value: 3,
+                  message: "Alias must be minimum 3 characters long",
+                },
+              }}
+            />
+            <CustomInput
+              defaultValue={user?.phoneNr}
+              name='phoneNr'
+              placeholder='Phone number'
+              control={control}
+              keyboardType={"numeric"}
+              rules={{
+                pattern: { value: /^[+]?\d{8,12}$/, message: "Must be valid phone number" },
+              }}
+            />
+            <CustomInput
+              defaultValue={user?.contactEmail}
+              name='contactEmail'
+              placeholder='Email'
+              control={control}
+              keyboardType={"default"}
+              rules={{
+                pattern: { value: EMAIL_REGEX, message: "Must be a valid email adress" },
+              }}
+            />
+            <CustomInput
+              defaultValue={user?.profilePictureUrl}
+              placeholder='Profile picture url'
+              name='profilePictureUrl'
+              keyboardType={"url"}
+              control={control}
+              rules={{
+                pattern: { value: IMGURL_REGEX, message: "Must be a valid url adress" },
+              }}
+            />
+            <View style={{ justifyContent: "center", marginLeft: "auto", marginRight: "auto" }}>
+              <CustomButton text='Update profile' onPress={handleSubmit(onSubmit)} />
+              <CustomButton
+                text='Discard changes'
+                bgColor='#c50f1f'
+                onPress={() => setEditMode(false)}
+              />
+            </View>
+          </View>
         ) : (
-          <Button mode='contained' buttonColor='#d0a753' onPress={handleLogOut}>
-            Logout
-          </Button>
+          <View>
+            <Title>{user?.alias}</Title>
+            {user?.contactEmail && <Text variant='titleSmall'>Email: {user?.contactEmail}</Text>}
+            {user?.phoneNr && <Text variant='titleSmall'>Phone number: {user?.phoneNr}</Text>}
+          </View>
         )}
-        <Button mode='contained' onPress={() => setEditMode((prevState) => !prevState)}>
-          Edit profile
-        </Button>
-      </View>
-    </View>
+
+        <View style={styles.buttonContainer}>
+          {editMode ? (
+            <Button mode='contained' buttonColor='#c50f1f' onPress={deleteAccount}>
+              Delete account
+            </Button>
+          ) : (
+            <Button mode='contained' buttonColor='#d0a753' onPress={handleLogOut}>
+              Logout
+            </Button>
+          )}
+          <Button mode='contained' onPress={() => setEditMode((prevState) => !prevState)}>
+            Edit profile
+          </Button>
+        </View>
+      </ScrollView>
+    </Ios_extraKeyboardPadding>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    marginTop: 10,
-    height: "95%",
-    width: "95%",
-    marginLeft: "auto",
-    marginRight: "auto",
+    padding: 20,
   },
   profilePicture: {
     height: 200,
